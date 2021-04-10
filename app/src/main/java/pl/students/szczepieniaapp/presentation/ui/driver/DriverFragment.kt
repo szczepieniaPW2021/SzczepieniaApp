54896d0940b1dfd27649a9f4fc7ca54dd8f47f13
lukasz <lukasz.birski@gmail.com>
2021-04-10 21:33:19 +0200

    Added google map to driver fragment

--- app/src/main/java/pl/students/szczepieniaapp/presentation/ui/driver/DriverFragment.kt
+++ app/src/main/java/pl/students/szczepieniaapp/presentation/ui/driver/DriverFragment.kt
@@ -8,15 +8,56 @@ import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import pl.students.szczepieniaapp.databinding.DriverFragmentBinding
 import pl.students.szczepieniaapp.presentation.MyFragment
+import com.google.android.gms.maps.GoogleMap
 
 @AndroidEntryPoint
 class DriverFragment : MyFragment<DriverFragmentBinding>() {
 
     private val viewModel : DriverViewModel by viewModels()
+    private var map: GoogleMap? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewModel.selectContext(activity)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView?.onSaveInstanceState(outState)
     }
 
     override fun onCreateView(
