bce5f4ad393df0f547fa0ec382498063e9364f15
lukasz <lukasz.birski@gmail.com>
2021-05-08 13:33:47 +0200

    created ConnectionLiveData and MyConnectionActivity

--- app/src/main/java/pl/students/szczepieniaapp/MainActivity.kt
+++ app/src/main/java/pl/students/szczepieniaapp/MainActivity.kt
@@ -1,51 +1,37 @@
 package pl.students.szczepieniaapp
 
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
+import pl.students.szczepieniaapp.presentation.util.MyConnectivityManager
+import javax.inject.Inject
+
+val TAG = "c-manager"
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    val TAG = "c-manager"
-
-    lateinit var cm: ConnectivityManager
-
-    val networkRequest: NetworkRequest = NetworkRequest.Builder()
-        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-        .build()
-
-    private val networkCallback = object : ConnectivityManager.NetworkCallback(){
-
-        override fun onAvailable(network: Network) {
-            super.onAvailable(network)
-            Log.d(TAG, "onAvailable: $network")
-        }
-
-        override fun onLost(network: Network) {
-            super.onLost(network)
-            Log.d(TAG, "onLost: $network")
-        }
-    }
+    @Inject
+    lateinit var connectivityManager: MyConnectivityManager
 
     override fun onStart() {
         super.onStart()
-        cm = this.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
-        cm.registerNetworkCallback(networkRequest, networkCallback)
+        connectivityManager.registerConnectionObserver(this)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        cm.unregisterNetworkCallback(networkCallback)
+        connectivityManager.unregisterConnectionObserver(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectivityManager.isNetworkAvailable.observe(this, {
+            Log.d(TAG, "onCreate: IS INTERNET AVAILABLE? $it")
+        })
+
     }
 }
