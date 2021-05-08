009f7c8a8d79ee7a92816668e7fc4b24f1236126
lukasz <lukasz.birski@gmail.com>
2021-05-08 11:51:28 +0200

    created network monitor in MainActivity

--- app/src/main/java/pl/students/szczepieniaapp/MainActivity.kt
+++ app/src/main/java/pl/students/szczepieniaapp/MainActivity.kt
@@ -1,11 +1,49 @@
 package pl.students.szczepieniaapp
 
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "c-manager"
+
+    lateinit var cm: ConnectivityManager
+
+    val networkRequest: NetworkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .build()
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback(){
+
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            Log.d(TAG, "onAvailable: $network")
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            Log.d(TAG, "onLost: $network")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        cm = this.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        cm.registerNetworkCallback(networkRequest, networkCallback)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cm.unregisterNetworkCallback(networkCallback)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
