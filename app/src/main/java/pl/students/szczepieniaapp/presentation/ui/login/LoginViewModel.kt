6d34b1b861adb9b4e54e8f3986a1081f9e4a4891
lukasz <lukasz.birski@gmail.com>
2021-05-09 20:47:01 +0200

    added textView with information about app version in LoginFragment

--- app/src/main/java/pl/students/szczepieniaapp/presentation/ui/login/LoginViewModel.kt
+++ app/src/main/java/pl/students/szczepieniaapp/presentation/ui/login/LoginViewModel.kt
@@ -3,6 +3,7 @@ package pl.students.szczepieniaapp.presentation.ui.login
 import android.view.View
 import androidx.navigation.Navigation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import pl.students.szczepieniaapp.BuildConfig
 import pl.students.szczepieniaapp.R
 import pl.students.szczepieniaapp.presentation.MyViewModel
 import javax.inject.Inject
@@ -21,4 +22,9 @@ constructor(
     fun goToDriverFragment(view: View) {
         Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_driverFragment)
     }
+
+    fun getVersion(): String {
+        return if (BuildConfig.DEBUG) "${BuildConfig.VERSION_NAME}.${BuildConfig.BUILD_TYPE} (${BuildConfig.VERSION_CODE})" else "${BuildConfig.VERSION_NAME})"
+    }
+
 }
