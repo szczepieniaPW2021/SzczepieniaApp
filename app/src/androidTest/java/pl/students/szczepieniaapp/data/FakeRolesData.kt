5b75ca2a30dee79c56046b0def3f7540c52f69eb
lukasz <lukasz.birski@gmail.com>
2021-05-22 15:47:35 +0200

    created UI tests for login fragment

---
+++ app/src/androidTest/java/pl/students/szczepieniaapp/data/FakeRolesData.kt
@@ -0,0 +1,13 @@
+package pl.students.szczepieniaapp.data
+
+object FakeRolesData {
+    val data = arrayListOf(
+        "Zaloguj się jako:",
+        "Pacjent",
+        "Kierowca",
+        "Lekarz",
+        "Operator logistyczny",
+        "Kierownik placówki",
+        "Operator NFZ"
+    )
+}
--- app/src/androidTest/java/pl/students/szczepieniaapp/presentation/ui/login/LoginFragmentTest.kt
+++ app/src/androidTest/java/pl/students/szczepieniaapp/presentation/ui/login/LoginFragmentTest.kt
@@ -4,18 +4,26 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.MediumTest
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.core.IsNot.not
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mockito.*
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import pl.students.szczepieniaapp.MainActivity.Companion.buildToastMessage
 import pl.students.szczepieniaapp.R
+import pl.students.szczepieniaapp.data.FakeRolesData
+import pl.students.szczepieniaapp.data.ToastFakeData
 import pl.students.szczepieniaapp.launchFragmentInHiltContainer
-import androidx.test.espresso.assertion.ViewAssertions.matches
+import pl.students.szczepieniaapp.presentation.util.ToastMatcher
+
 
 @MediumTest
 @HiltAndroidTest
@@ -35,24 +43,38 @@ class LoginFragmentTest {
 
         launchFragmentInHiltContainer<LoginFragment> {}
 
+        //checks if welcome textView is displayed
+        onView(withText(R.string.welcome_text)).check(
+            matches(isDisplayed())
+        )
+
         //checks if title is displayed
         onView(withText(R.string.login_fragment)).check(
             matches(isDisplayed())
         )
 
         //checks if button navigating to patient fragment is displayed
-        onView(withText(R.string.go_to_patient)).check(
+        onView(withId(R.id.spinner)).check(
             matches(isDisplayed())
         )
 
-        //checks if button navigating to driver fragment is displayed
-        onView(withText(R.string.go_to_driver)).check(
+        //checks if button navigating to other fragments is displayed
+        onView(withText(R.string.login_text)).check(
             matches(isDisplayed())
         )
 
         //checks if textView with app version is displayed
         onView(withId(R.id.versionTextView)).check(
             matches(isDisplayed())
+        )
+
+        onView(withSpinnerText("Zaloguj się jako:")).check(
+            matches(isDisplayed())
+        )
+
+        //checks if button is disabled
+        onView(withText(R.string.login_text)).check(
+            matches(not(isEnabled()))
         )
     }
 
@@ -65,10 +87,25 @@ class LoginFragmentTest {
             Navigation.setViewNavController(requireView(), navController)
         }
 
-        onView(withId(R.id.patientNavBtn)).perform(click())
+        //click spinner to select role
+        onView(withId(R.id.spinner)).perform(
+            click()
+        )
+
+        //click on role "pacjent"
+        onData(`is`(FakeRolesData.data[1])).perform(click())
+
+        //checks if button is disabled
+        onView(withText(R.string.login_text)).check(
+            matches(isEnabled())
+        )
+
+        //click navbutton
+        onView(withId(R.id.navBtn)).perform(click())
 
         pressBackUnconditionally()
 
+        //checks if navigated to patient fragment
         verify(navController).navigate(
             R.id.action_loginFragment_to_patientFragment
         )
@@ -83,13 +120,103 @@ class LoginFragmentTest {
             Navigation.setViewNavController(requireView(), navController)
         }
 
-        onView(withId(R.id.driverNavBtn)).perform(click())
+        //click spinner to select role
+        onView(withId(R.id.spinner)).perform(
+            click()
+        )
+
+        //click on role "kierowca"
+        onData(`is`(FakeRolesData.data[2])).perform(click())
+
+        //checks if button is disabled
+        onView(withText(R.string.login_text)).check(
+            matches(isEnabled())
+        )
+
+        //click navbutton
+        onView(withId(R.id.navBtn)).perform(click())
 
         pressBackUnconditionally()
 
+        //checks if navigated to patient fragment
         verify(navController).navigate(
             R.id.action_loginFragment_to_driverFragment
         )
     }
 
+    @Test
+    fun clickZalogujSieJakoInSpinnerInSpinner_DisableButton() {
+
+        launchFragmentInHiltContainer<LoginFragment> {}
+
+        //click spinner to select role
+        onView(withId(R.id.spinner)).perform(
+            click()
+        )
+
+        //click on role "zaloguj się jako:"
+        onData(`is`(FakeRolesData.data[0])).perform(click())
+
+        //checks if button is disabled
+        onView(withText(R.string.login_text)).check(
+            matches(not(isEnabled()))
+        )
+
+    }
+
+    @Test
+    fun clickLekarzInSpinner_DisableButtonAndShowToast() {
+
+        launchFragmentInHiltContainer<LoginFragment> {}
+
+        //click spinner to select role
+        onView(withId(R.id.spinner)).perform(
+            click()
+        )
+
+        //click on role "lekarz"
+        onData(`is`(FakeRolesData.data[3])).perform(click())
+
+        //checks if button is disabled
+        onView(withText(R.string.login_text)).check(
+            matches(not(isEnabled()))
+        )
+
+        //checks if toast is displayed and the message is correct
+        onView(withText(buildToastMessage(ToastFakeData.data))).inRoot(ToastMatcher())
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun clickZalogujSieAfterClickingPacjentInSpinnerInSpinner_DisableButton() {
+
+        launchFragmentInHiltContainer<LoginFragment> {}
+
+        //click spinner to select role
+        onView(withId(R.id.spinner)).perform(
+            click()
+        )
+
+        //click on role "pacjent"
+        onData(`is`(FakeRolesData.data[1])).perform(click())
+
+        //checks if button is disabled
+        onView(withText(R.string.login_text)).check(
+            matches(isEnabled())
+        )
+
+        //click spinner to select role
+        onView(withId(R.id.spinner)).perform(
+            click()
+        )
+
+        //click on role "zaloguj się jako:"
+        onData(`is`(FakeRolesData.data[0])).perform(click())
+
+        //checks if button is disabled
+        onView(withText(R.string.login_text)).check(
+            matches(not(isEnabled()))
+        )
+
+    }
 }
