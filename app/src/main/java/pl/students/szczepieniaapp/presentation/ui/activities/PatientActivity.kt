d9f0636ef0152a868be595edbb4e290cf3beaa63
lukasz <lukasz.birski@gmail.com>
2021-05-23 18:06:16 +0200

    created navigation in PatientActivity

--- app/src/main/java/pl/students/szczepieniaapp/presentation/ui/activities/PatientActivity.kt
+++ app/src/main/java/pl/students/szczepieniaapp/presentation/ui/activities/PatientActivity.kt
@@ -3,9 +3,12 @@ package pl.students.szczepieniaapp.presentation.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import pl.students.szczepieniaapp.R
-import pl.students.szczepieniaapp.databinding.ActivityMainBinding
 import pl.students.szczepieniaapp.databinding.ActivityPatientBinding
 import pl.students.szczepieniaapp.presentation.util.MyConnectivityManager
 import javax.inject.Inject
@@ -32,6 +35,8 @@ class PatientActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPatientBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.patientBottomNav.setupWithNavController(Navigation.findNavController(this, R.id.activityPatientFragmentContainerView))
 
         connectivityManager.isNetworkAvailable.observe(this, {
             if (it) binding.noConnectionBanner.visibility = View.GONE else binding.noConnectionBanner.visibility = View.VISIBLE
