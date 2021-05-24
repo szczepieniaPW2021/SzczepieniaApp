fbf2dea20d0ab968c1ce914cd273573abfb5b439
lukasz <lukasz.birski@gmail.com>
2021-05-24 20:41:16 +0200

    added calendar to PatientCalendarFragment

--- app/src/main/java/pl/students/szczepieniaapp/presentation/ui/fragment/PatientCalendarFragment.kt
+++ app/src/main/java/pl/students/szczepieniaapp/presentation/ui/fragment/PatientCalendarFragment.kt
@@ -2,16 +2,19 @@ package pl.students.szczepieniaapp.presentation.ui.fragment
 
 import android.R
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import pl.students.szczepieniaapp.databinding.PatientCalendarFragmentBinding
 import pl.students.szczepieniaapp.presentation.MyFragment
 import pl.students.szczepieniaapp.presentation.ui.viewmodel.PatientCalendarViewModel
+import java.util.*
 
 @AndroidEntryPoint
 class PatientCalendarFragment : MyFragment<PatientCalendarFragmentBinding>() {
@@ -25,19 +28,44 @@ class PatientCalendarFragment : MyFragment<PatientCalendarFragmentBinding>() {
     ): View? {
         _binding = PatientCalendarFragmentBinding.inflate(inflater, container, false)
         binding.viewmodel = viewModel
-        setSelectCitySpinner()
         viewModel.apply {
+            cities.observe(viewLifecycleOwner){
+                setSpinner(it as List<Objects>, binding.selectCitySpinner)
+            }
+            facilities.observe(viewLifecycleOwner){
+                setSpinner(it as List<Objects>, binding.selectFacilitySpinner)
+            }
+
             binding.selectCitySpinner.onItemSelectedListener = this
+            binding.selectFacilitySpinner.onItemSelectedListener = this
+
+            isFacilitySpinnerVisible.observe(viewLifecycleOwner){
+                if (it) {
+                    binding.selectFacilityRelativeLayout.visibility = View.VISIBLE
+                    binding.selectFacilityTextView.visibility = View.VISIBLE
+                } else {
+                    binding.selectFacilityTextView.visibility = View.INVISIBLE
+                    binding.selectFacilityRelativeLayout.visibility = View.INVISIBLE
+                }
+            }
+
+            isCalendarVisible.observe(viewLifecycleOwner){
+                binding.calendarView.minDate = viewModel.getCurrentTime()
+                binding.calendarView.maxDate = viewModel.setMaxDate()
+                if (it) binding.calendarView.visibility = View.VISIBLE else binding.calendarView.visibility = View.INVISIBLE
+            }
+
+            viewModel.getDate(binding.calendarView)
         }
         return binding.root
     }
 
-    private fun setSelectCitySpinner() {
-        val spinner: Spinner = binding.selectCitySpinner
+    private fun setSpinner(list: List<Objects>, spinner: Spinner) {
+        val spinner: Spinner = spinner
         ArrayAdapter(
             requireContext(),
             R.layout.simple_spinner_item,
-            viewModel.fetchCities()
+            list
         ).also { adapter ->
             adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
             spinner.adapter = adapter
--- app/src/main/java/pl/students/szczepieniaapp/presentation/ui/viewmodel/PatientCalendarViewModel.kt
+++ app/src/main/java/pl/students/szczepieniaapp/presentation/ui/viewmodel/PatientCalendarViewModel.kt
@@ -1,10 +1,17 @@
 package pl.students.szczepieniaapp.presentation.ui.viewmodel
 
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.CalendarView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import pl.students.szczepieniaapp.R
 import pl.students.szczepieniaapp.presentation.MyViewModel
+import java.util.*
 import javax.inject.Inject
+import kotlin.collections.ArrayList
 
 @HiltViewModel
 class PatientCalendarViewModel
@@ -13,8 +20,26 @@ constructor(
 
 ) : MyViewModel(), AdapterView.OnItemSelectedListener {
 
+    private val _cities = MutableLiveData<ArrayList<String>>()
+    val cities: LiveData<ArrayList<String>> get() = _cities
 
-    fun fetchCities(): ArrayList<String> {
+    private val _facilities = MutableLiveData<ArrayList<String>>()
+    val facilities: LiveData<ArrayList<String>> get() = _facilities
+
+    private val _isFacilitySpinnerVisible = MutableLiveData<Boolean>()
+    val isFacilitySpinnerVisible: LiveData<Boolean> get() = _isFacilitySpinnerVisible
+
+    private val _isCalendarVisible = MutableLiveData<Boolean>()
+    val isCalendarVisible: LiveData<Boolean> get() = _isCalendarVisible
+
+    private var selectedCity: String? = null
+    private var selectedFacility: String? = null
+
+    init {
+        _cities.postValue(fetchCities())
+    }
+
+    private fun fetchCities(): ArrayList<String> {
         val data: ArrayList<String> = arrayListOf()
         data.add("Miasto:")
         data.add("Warszawa")
@@ -24,11 +49,77 @@ constructor(
         return data
     }
 
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    private fun fetchFacilities(): ArrayList<String> {
+        val data: ArrayList<String> = arrayListOf()
+        data.add("Punkt:")
+        data.add("Punkt 1")
+        data.add("Punkt 2")
+        data.add("Punkt 3")
+        data.add("Punkt 4")
+        return data
+    }
 
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Log.d(PatientCalendarViewModel::class.java.name, "selected: ${parent!!.adapter.getItem(position)}")
+        if (parent!!.id == R.id.selectCitySpinner){
+            selectCity(parent!!.adapter.getItem(position) as String, view)
+        } else if (parent!!.id == R.id.selectFacilitySpinner) {
+            selectFacility(parent!!.adapter.getItem(position) as String, view)
+        }
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
+        Log.d(PatientCalendarViewModel::class.java.name, "onNothingSelected:")
+    }
 
+    private fun selectCity(item: String, view: View?) {
+        when (item) {
+
+            "Miasto:" -> {
+                selectedCity = null
+                selectedFacility = null
+                _isFacilitySpinnerVisible.postValue(false)
+                _isCalendarVisible.postValue(false)
+            }
+
+            else -> {
+                _facilities.postValue(fetchFacilities())
+                selectedCity = item
+                _isFacilitySpinnerVisible.postValue(true)
+                _isCalendarVisible.postValue(false)
+            }
+        }
+    }
+
+    private fun selectFacility(item: String, view: View?) {
+        when (item) {
+
+            "Punkt:" -> {
+                selectedFacility = null
+                _isCalendarVisible.postValue(false)
+            }
+
+            else -> {
+                selectedCity = item
+                _isCalendarVisible.postValue(true)
+            }
+        }
+    }
+
+    fun getCurrentTime() : Long {
+        val calendar: Calendar = Calendar.getInstance()
+        return calendar.timeInMillis
+    }
+
+    fun setMaxDate() : Long {
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.add(Calendar.MONTH, 2)
+        return calendar.timeInMillis
+    }
+
+    fun getDate(calendarView: CalendarView) {
+        calendarView.setOnDateChangeListener {_, year, month, dayOfMonth ->
+            Log.d("testuje", "getDate: $year, $month, $dayOfMonth")
+        }
     }
 }
